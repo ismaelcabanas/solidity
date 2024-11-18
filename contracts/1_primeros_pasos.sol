b99253114232a7f039f72b87a57adcc6ff875f13
ismaelcabanas <ismaelcabanas@gmail.com>
2024-11-18 19:05:28 +0100

    Smart contract definition

--- contracts/1_primeros_pasos.sol
+++ contracts/1_primeros_pasos.sol
@@ -1,1 +1,10 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// Import smart contract
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// Smart contract declaration
+contract FirstContract is ERC721 {
+
+}
